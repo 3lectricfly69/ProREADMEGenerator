77e12afe848aaf733d70d7a5a97eb121399ea88f
Marlie Ford <jive.hive@outlook.com>
2023-10-24 17:50:58 -0500

    actually complete now

--- index.js
+++ index.js
@@ -59,8 +59,6 @@ ${testInstructions}
 
 const init = () => {
     promptUser()
-      // Use writeFile method imported from fs.promises to use promises instead of
-      // a callback function
       .then((answers) => writeFile('README.md', generateREADME(answers)))
       .then(() => console.log('Successfully wrote to README.md'))
       .catch((err) => console.error(err));
