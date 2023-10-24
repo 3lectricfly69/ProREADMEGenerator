f5c45dcfa7f8400017729d46ac08342ee9f6a262
Marlie Ford <jive.hive@outlook.com>
2023-10-24 17:43:20 -0500

    after adding a full prompt and README data, messages appear in the terminal for user's input/responses to generate a professional README file.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 
-Project title: Marlie's README prompt
+Project title: ProREADMEGenerator
 
+Description: 
+When prompted for information about the user's application repository, a README file is generated with the messages and the user's responses.
+
+Installation Instructions:
+To ensure that this program will work properly, run npm install in the command line of the terminal in the root of this project.
+
+Usage Information:
+Title, Description, Installation Instructions, Usage Information, Contribution Guidelines, and Test Instructions
+
+Contribution Guidelines
+Users may contribute content to the project by entering responses in the command line.
+
+Test Instructions
+After opening a terminal in the root of the project, run node fileName.js in the command line. Then, open the generated README file where in which the messages and their given responses will display.
 
--- index.js
+++ index.js
@@ -9,12 +9,51 @@ const promptUser = () => {
             name: 'title',
             message: 'What is the title of your project?',
         },
+        {
+            type:'input',
+            name: 'description',
+            message: 'Describe the functionality of this project.',
+        },
+        {
+            type: 'input',
+            name: 'installationInstructions',
+            message: 'What are the installation instructions that will be included for said user?',
+        },
+        {
+            type: 'input',
+            name: 'usageInformation',
+            message: 'Give an outline of the information data that is automatically collected through the use of your application.',
+        },
+        {
+            type: 'input',
+            name: 'contributionGuidelines',
+            message: 'Explain how others may contribute user-generated content to the project.',
+        },
+        {
+            type: 'input',
+            name: 'testInstructions',
+            message: 'Include instructions for the test here.',
+        },
     ])
 }
-const generateREADME = ({title}) =>
+const generateREADME = ({title, description, installationInstructions, usageInformation, contributionGuidelines, testInstructions}) =>
 `
 Project title: ${title}
 
+Description: 
+${description}
+
+Installation Instructions:
+${installationInstructions}
+
+Usage Information:
+${usageInformation}
+
+Contribution Guidelines
+${contributionGuidelines}
+
+Test Instructions
+${testInstructions}
 
 `;
 
